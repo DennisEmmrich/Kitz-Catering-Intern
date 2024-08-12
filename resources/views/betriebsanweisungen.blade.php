acce8973e7af10dfa11872fb1fe99bf354c573df
dennisemmrich <kontakt@dennisemmrich.de>
2024-08-12 22:16:59 +0200

    wip on "suchleiste" on betriebsanweisungen.blade.php

--- resources/views/betriebsanweisungen.blade.php
+++ resources/views/betriebsanweisungen.blade.php
@@ -2,33 +2,6 @@
 
     <x-section link="welcome">
 
-        <div class="grid grid-cols-1">
-
-            @foreach($operatingInstructions as $operatingInstruction)
-            <!-- modal -->
-            <x-modal>
-                <x-slot:button>
-                    <x-list-item>
-                        {{ $operatingInstruction['title'] }}
-                    </x-list-item>
-                </x-slot:button>
-
-                <!--content slot -->
-                <x-iframe :path="getAsset($operatingInstruction['file']['_id'])"/>
-                <!--end content slot -->
-            </x-modal>
-            @endforeach
-
-        </div>
-
-    </x-section>
-
-</x-layout>
-
-<x-layout pageTitle="Kitz-Catering / Betriebsanweisungen">
-
-    <x-section link="welcome">
-
         <!-- Suchleiste -->
         <div class="w-full mb-6 flex relative">
             <input
